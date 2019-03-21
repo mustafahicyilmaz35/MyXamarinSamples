648f175f5d047e020fc274eb2e85eb09463a7bb3
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2019-03-21 21:46:52 +0300

    Add files via upload

---
+++ XamarinSqlitepcl/XamarinSqlitepcl/XamarinSqlitepcl/DataAccess/Database.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using XamarinSqlitepcl.Models;
+
+namespace XamarinSqlitepcl.DataAccess
+{
+    public class Database
+    {
+		//database bağlantısını asenkron olarak oluşturacağız.
+		readonly SQLiteAsyncConnection _database;
+
+		//const un içinde database bağlantısını oluşturup, daha sonra o bağlantı nesnesi ile model listesi oluşturuyoruz.
+		public Database(string dbPath)
+		{
+			_database = new SQLiteAsyncConnection(dbPath);
+			_database.CreateTableAsync<Person>().Wait();
+		}
+
+		//Sorgulanan verilerin listelenmesi işi yapılacak.
+		public Task<List<Person>> GetPeopleAsync()
+		{
+			return _database.Table<Person>().ToListAsync();
+		}
+
+		//Veritabanına yeni bir kişi veri ekleyeceğiz.
+		public Task<int> SavePersonAsync(Person person)
+		{
+			if(person.ID!=0)
+			{
+				return _database.UpdateAsync(person);
+			}
+			else
+			{
+				return _database.InsertAsync(person);
+			}
+			
+		}
+
+		public async Task<int> DeleteAsync(Person person)
+		{
+			return await _database.DeleteAsync(person);
+		}
+
+
+
+    }
+}
---
+++ XamarinSqlitepcl/XamarinSqlitepcl/XamarinSqlitepcl/Views/PersonView.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinSqlitepcl.Models;
+
+namespace XamarinSqlitepcl.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PersonView : ContentPage
+	{
+
+
+		private int selectedPersonId;
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			lst_Person.ItemsSource = await App.Database.GetPeopleAsync();
+		}
+
+		private void onItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			Person selectedPerson = e.SelectedItem as Person;
+			selectedPersonId = selectedPerson.ID;
+		}
+
+		private async void OnAddButtonClicked(object sender, EventArgs e)
+		{
+			if(!string.IsNullOrWhiteSpace(nameEntry.Text) && !string.IsNullOrWhiteSpace(ageEntry.Text))
+			{
+				await App.Database.SavePersonAsync(new Person { Name = nameEntry.Text, Age=int.Parse(ageEntry.Text) });
+
+				nameEntry.Text = ageEntry.Text = string.Empty;
+				lst_Person.ItemsSource = await App.Database.GetPeopleAsync();
+			}
+		}
+		private async void OnUpdateButtonClicked(object sender, EventArgs e)
+		{
+
+			//var id = ((sender as Button).BindingContext as Person).ID;
+			await App.Database.SavePersonAsync(new Person { ID=selectedPersonId, Name = nameEntry.Text, Age = int.Parse(ageEntry.Text) });
+
+			
+			lst_Person.ItemsSource = await App.Database.GetPeopleAsync();
+			
+		}
+
+		private async void onDeleteClicked(object sender, EventArgs e)
+		{
+			await App.Database.DeleteAsync(new Person {ID = selectedPersonId });
+			lst_Person.ItemsSource = await App.Database.GetPeopleAsync();
+		}
+
+		
+
+		public PersonView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
