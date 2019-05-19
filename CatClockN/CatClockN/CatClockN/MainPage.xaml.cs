d49468a9573e4dc1c9d9da7bfafa1ecf94151c87
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2019-05-19 20:46:06 +0300

    Add files via upload

---
+++ CatClockN/CatClockN/CatClockN/MainPage.xaml.cs
@@ -0,0 +1,198 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CatClockN
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    //[DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+
+        SKPaint blackFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Black
+        };
+        SKPaint whiteStrokePaint = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            Color = SKColors.White,
+            StrokeCap = SKStrokeCap.Round,
+            IsAntialias = true
+        };
+        SKPaint whiteFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.White
+        };
+
+        SKPaint blackStrokePaint = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            Color = SKColors.Black,
+            StrokeWidth = 20,
+            IsAntialias = true
+        };
+
+        SKPaint greenFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.PaleGreen
+        };
+
+        SKPaint grayFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Gray
+        };
+
+        SKPaint backgroundFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill
+        };
+
+        SKPath catEarPath = new SKPath();
+        SKPath catEyePath = new SKPath();
+        SKPath catPupilPath = new SKPath();
+        SKPath catTailPath = new SKPath();
+
+        SKPath hourHandPath = SKPath.ParseSvgPathData(
+            "M 0 -60 C 0 -30 20 -30 5 -20 L 5 0 C 5 7.5 -5 7.5 -5 0 L -5 -20 C -20 -30 0 -30 0 -60");
+        SKPath minuteHandPath = SKPath.ParseSvgPathData(
+            "M 0 -80 C 0 -75 0 -70 2.5 -60 L 2.5 0 C 2.5 5 -2.5 5 -2.5 0 L -2.5 -60 C 0 -70 0 -75 0 -80");
+
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Kedinin kulağı
+            catEarPath.MoveTo(0, 0);
+            catEarPath.LineTo(0, 75);
+            catEarPath.LineTo(100, 75);
+            catEarPath.Close();
+
+            //Kedinin Gözü
+            catEyePath.MoveTo(0, 0);
+            catEyePath.ArcTo(50, 50, 0, SKPathArcSize.Small, SKPathDirection.Clockwise, 50, 0);
+            catEyePath.ArcTo(50, 50, 0, SKPathArcSize.Small, SKPathDirection.Clockwise, 0, 0);
+            catEyePath.Close();
+
+            // Kedinin Kuyruğu
+            catTailPath.MoveTo(0, 100);
+            catTailPath.CubicTo(50, 200, 0, 250, -50, 200);
+
+            //Gölge
+            Assembly assembly = GetType().GetTypeInfo().Assembly;
+            using (Stream stream = assembly.GetManifestResourceStream("CatClockN.WoodGrain.png"))
+            using (SKManagedStream skStream = new SKManagedStream(stream))
+            using (SKBitmap bitmap = SKBitmap.Decode(skStream))
+            using (SKShader shader = SKShader.CreateBitmap(bitmap, SKShaderTileMode.Mirror, SKShaderTileMode.Mirror))
+            {
+                backgroundFillPaint.Shader = shader;
+            }
+
+            Device.StartTimer(TimeSpan.FromSeconds(1f / 60), () =>
+            {
+                canvasView.InvalidateSurface();
+                return true;
+            });
+        }
+
+        private void canvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.DrawPaint(backgroundFillPaint);
+
+            int witdh = e.Info.Width;
+            int height = e.Info.Height;
+
+            canvas.Translate(witdh / 2, height / 2);
+            canvas.Scale(Math.Min(witdh / 210f,height/520f));
+
+            DateTime dateTime = DateTime.Now;
+
+            canvas.DrawCircle(0, -160, 75, blackFillPaint);
+
+            for (int i = 0; i < 2; i++)
+            {
+                canvas.Save();
+                canvas.Scale(2 * i - 1, 1);
+
+                canvas.Save();
+                canvas.Translate(-65, -255);
+                canvas.DrawPath(catEarPath, blackFillPaint);
+                canvas.Restore();
+
+                canvas.Save();
+                canvas.Translate(10, -170);
+                canvas.DrawPath(catEyePath, greenFillPaint);
+                canvas.DrawPath(catPupilPath, blackFillPaint);
+                canvas.Restore();
+
+                // Draw whiskers
+                canvas.DrawLine(10, -120, 100, -100, whiteStrokePaint);
+                canvas.DrawLine(10, -125, 100, -120, whiteStrokePaint);
+                canvas.DrawLine(10, -130, 100, -140, whiteStrokePaint);
+                canvas.DrawLine(10, -135, 100, -160, whiteStrokePaint);
+
+                canvas.Restore();
+            }
+
+
+            float t = (float)Math.Sin((dateTime.Second % 2 + dateTime.Millisecond / 1000.0) * Math.PI);
+            catTailPath.Reset();
+            catTailPath.MoveTo(0, 100);
+            SKPoint point1 = new SKPoint(-50 * t, 200);
+            SKPoint point2 = new SKPoint(0, 250 - Math.Abs(50 * t));
+            SKPoint point3 = new SKPoint(50 * t, 250 - Math.Abs(75 * t));
+            catTailPath.CubicTo(point1, point2, point3);
+
+            canvas.DrawPath(catTailPath, blackStrokePaint);
+
+            // Clock background
+            canvas.DrawCircle(0, 0, 100, blackFillPaint);
+
+            // Hour and minute marks
+            for (int angle = 0; angle < 360; angle += 6)
+            {
+                canvas.DrawCircle(0, -90, angle % 30 == 0 ? 4 : 2, whiteFillPaint);
+                canvas.RotateDegrees(6);
+            }
+
+            canvas.Save();
+            canvas.RotateDegrees(30 * dateTime.Hour + dateTime.Minute / 2f);
+            canvas.DrawPath(hourHandPath, grayFillPaint);
+            canvas.DrawPath(hourHandPath, whiteStrokePaint);
+            canvas.Restore();
+
+            canvas.Save();
+            canvas.RotateDegrees(6 * dateTime.Minute + dateTime.Second / 10f);
+            canvas.DrawPath(minuteHandPath, grayFillPaint);
+            canvas.DrawPath(minuteHandPath, whiteStrokePaint);
+            canvas.Restore();
+
+            canvas.Save();
+            float seconds = dateTime.Second + dateTime.Millisecond / 1000f;
+            canvas.RotateDegrees(6 * seconds);
+            whiteStrokePaint.StrokeWidth = 2;
+            canvas.DrawLine(0, 10, 0, -80, whiteStrokePaint);
+            canvas.Restore();
+        }
+
+    }
+}
